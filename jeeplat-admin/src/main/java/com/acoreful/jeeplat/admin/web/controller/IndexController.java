82e1a40cdd0db38b8c74d6b28e7f493b147ecfd6
WallenHeng <acoresoft@163.com>
2018-06-18 19:54:20 +0800

    新增github push

--- jeeplat-admin/src/main/java/com/acoreful/jeeplat/admin/web/controller/IndexController.java
+++ jeeplat-admin/src/main/java/com/acoreful/jeeplat/admin/web/controller/IndexController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping
+@Slf4j
 public class IndexController {
 	@GetMapping("index")
 	public String index(){
+		log.info("====================");
 		return "index";
 	}
 }
